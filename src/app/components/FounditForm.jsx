f5b8566e336a8a97855f4f15799fd7f2f8189f3a
SoumishDas <73273475+SoumishDas@users.noreply.github.com>
2025-06-12 15:19:13 +0530

    Integrate register form with API

--- src/app/components/FounditForm.jsx
+++ src/app/components/FounditForm.jsx
@@ -130,7 +130,7 @@ export default function FounditForm() {
 
             {/* Disclaimer Text */}
             <Typography variant="body2" color="textSecondary" sx={{ mt: 2 }}>
-              By clicking on ‘Continue’ I agree to Foundit's{' '}
+              By clicking on ‘Continue’ I agree to Foundit&apos;s{' '}
               <Link href="#" underline="hover">
                 Terms and Conditions
               </Link>
